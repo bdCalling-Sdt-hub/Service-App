40d085ec6cfaba55bfb09248aae281105d9b374c
sagorahamed1 <sagorahammed002@gmail.com>
2024-05-13 11:23:23 +0600

    provider bottom nav bar done

---
+++ lib/views/screen/Provider/provider_bottm_nav_bar/provider_bottom_nav_bar.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:service_app/views/screen/Provider/provider_home/provider_home_screen.dart';
+import 'package:service_app/views/screen/User/user_service/user_service_screen.dart';
+import 'package:service_app/views/screen/message/Message/message_screen.dart';
+import '../../../../utils/app_colors.dart';
+import '../../../../utils/app_icons.dart';
+import '../../Profile/profile_screen/profile_screen.dart';
+
+
+
+class ProviderBottomNavBar extends StatefulWidget {
+  const ProviderBottomNavBar({super.key});
+
+  @override
+  State<ProviderBottomNavBar> createState() => _BottomNavigationBarExampleState();
+}
+
+class _BottomNavigationBarExampleState extends State<ProviderBottomNavBar> {
+  int _selectedIndex = 0;
+
+
+  static final List _widgetOptions = [
+    ProviderHomeScreen(),
+    UserServiceScreen(),
+    MessageScreen(),
+    ProfileScreen(),
+  ];
+
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: _widgetOptions.elementAt(_selectedIndex),
+
+      ///------------------------bottom nav bar----------------------------?>
+      bottomNavigationBar: BottomNavigationBar(
+
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          ///---------------home---------------->
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+                AppIcons.homeBottomNavbar,
+                color: Colors.white
+            ),
+            label: 'Home',
+          ),
+
+          ///---------------events---------------->
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+                AppIcons.serviecIcon,
+                color: Colors.white
+            ),
+            label: 'Helps',
+          ),
+
+          ///---------------matches---------------->
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+                AppIcons.massegeIcon,
+                color: Colors.white
+            ),
+            label: 'Messages',
+          ),
+
+          ///---------------profile---------------->
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+                AppIcons.profileIconBottombar,
+                color: Colors.white
+            ),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        showUnselectedLabels: false,
+        iconSize: 20.h,
+        backgroundColor: AppColors.primaryColor,
+        selectedFontSize: 14.h,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white,
+
+      ),
+    );
+  }
+}
