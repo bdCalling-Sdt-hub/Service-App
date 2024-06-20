0c27d24d0b928a18bb978d364ac7147a3b2ed2d5
ebrahim48 <ebrahim.cse.bu@gmail.com>
2024-06-20 15:16:29 +0600

    more information

--- lib/models/more_information_model.dart
+++ lib/models/more_information_model.dart
@@ -1,165 +1,113 @@
-class MoreInformationModel {
-  String? status;
-  int? statusCode;
-  String? message;
-  Data? data;
-
-  MoreInformationModel({this.status, this.statusCode, this.message, this.data});
-
-  MoreInformationModel.fromJson(Map<String, dynamic> json) {
-    status = json['status'];
-    statusCode = json['statusCode'];
-    message = json['message'];
-    data = json['data'] != null ? Data.fromJson(json['data']) : null;
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = Map<String, dynamic>();
-    data['status'] = status;
-    data['statusCode'] = statusCode;
-    data['message'] = message;
-    if (this.data != null) {
-      data['data'] = this.data!.toJson();
-    }
-    return data;
-  }
-}
-
-class Data {
-  Attributes? attributes;
-
-  Data({this.attributes});
-
-  Data.fromJson(Map<String, dynamic> json) {
-    attributes = json['attributes'] != null
-        ? Attributes.fromJson(json['attributes'])
-        : null;
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = Map<String, dynamic>();
-    if (attributes != null) {
-      data['attributes'] = attributes!.toJson();
-    }
-    return data;
-  }
-}
-
-class Attributes {
-  String? id;
-  String? name;
-  String? email;
-  String? role;
-  String? phone;
-  String? address;
-  bool? privacyPolicyAccepted;
-  bool? isAdmin;
-  bool? isVerified;
-  bool? isDeleted;
-  bool? isBlocked;
-  List<Image>? image;
-  // List<DriverLicenceFront>? driverLicenceFront;
-  // List<DriverLicenceback>? driverLicenceback;
-  Null oneTimeCode;
-  int? iV;
-
-  Attributes(
-      {this.id,
-        this.name,
-        this.email,
-        this.role,
-        this.phone,
-        this.address,
-        this.privacyPolicyAccepted,
-        this.isAdmin,
-        this.isVerified,
-        this.isDeleted,
-        this.isBlocked,
-        this.image,
-        // this.driverLicenceFront,
-        // this.driverLicenceback,
-        this.oneTimeCode,
-        this.iV});
-
-  Attributes.fromJson(Map<String, dynamic> json) {
-    id = json['_id'];
-    name = json['name'];
-    email = json['email'];
-    role = json['role'];
-    phone = json['phone'];
-    address = json['address'];
-    privacyPolicyAccepted = json['privacyPolicyAccepted'];
-    isAdmin = json['isAdmin'];
-    isVerified = json['isVerified'];
-    isDeleted = json['isDeleted'];
-    isBlocked = json['isBlocked'];
-    if (json['image'] != null) {
-      image = <Image>[];
-      json['image'].forEach((v) {
-        image!.add(new Image.fromJson(v));
-      });
-    }
-    // if (json['driverLicenceFront'] != null) {
-    //   driverLicenceFront = <DriverLicenceFront>[];
-    //   json['driverLicenceFront'].forEach((v) {
-    //     driverLicenceFront!.add(new DriverLicenceFront.fromJson(v));
-    //   });
-    // }
-    // if (json['driverLicenceback'] != null) {
-    //   driverLicenceback = <DriverLicenceback>[];
-    //   json['driverLicenceback'].forEach((v) {
-    //     driverLicenceback!.add(new DriverLicenceback.fromJson(v));
-    //   });
-    // }
-    oneTimeCode = json['oneTimeCode'];
-    iV = json['__v'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['_id'] = this.id;
-    data['name'] = this.name;
-    data['email'] = this.email;
-    data['role'] = this.role;
-    data['phone'] = this.phone;
-    data['address'] = this.address;
-    data['privacyPolicyAccepted'] = this.privacyPolicyAccepted;
-    data['isAdmin'] = this.isAdmin;
-    data['isVerified'] = this.isVerified;
-    data['isDeleted'] = this.isDeleted;
-    data['isBlocked'] = this.isBlocked;
-    if (this.image != null) {
-      data['image'] = this.image!.map((v) => v.toJson()).toList();
-    }
-    // if (driverLicenceFront != null) {
-    //   data['driverLicenceFront'] =
-    //       driverLicenceFront!.map((v) => v.toJson()).toList();
-    // }
-    // if (driverLicenceback != null) {
-    //   data['driverLicenceback'] =
-    //       driverLicenceback!.map((v) => v.toJson()).toList();
-    // }
-    data['oneTimeCode'] = oneTimeCode;
-    data['__v'] = iV;
-    return data;
-  }
-}
-
-class Image {
-  String? publicFileUrl;
-  String? path;
-
-  Image({this.publicFileUrl, this.path});
-
-  Image.fromJson(Map<String, dynamic> json) {
-    publicFileUrl = json['publicFileUrl'];
-    path = json['path'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = Map<String, dynamic>();
-    data['publicFileUrl'] = publicFileUrl;
-    data['path'] = path;
-    return data;
-  }
-}
+// class MoreInformationModel {
+//   String? id;
+//   String? name;
+//   String? email;
+//   String? role;
+//   String? phone;
+//   String? address;
+//   bool? privacyPolicyAccepted;
+//   bool? isAdmin;
+//   bool? isVerified;
+//   bool? isDeleted;
+//   bool? isBlocked;
+//   List<Null>? image;
+//   List<Null>? driverLicenceFront;
+//   List<Null>? driverLicenceback;
+//   dynamic oneTimeCode;
+//   int? v;
+//   double? latitude;
+//   double? longitude;
+//
+//
+//   MoreInformationModel({this.id,
+//     this.name,
+//     this.email,
+//     this.role,
+//     this.phone,
+//     this.address,
+//     this.privacyPolicyAccepted,
+//     this.isAdmin,
+//     this.isVerified,
+//     this.isDeleted,
+//     this.isBlocked,
+//     this.image,
+//     this.driverLicenceFront,
+//     this.driverLicenceback,
+//     this.oneTimeCode,
+//     this.v,
+//     this.latitude,
+//     this.longitude});
+//
+//   factory MoreInformationModel.fromJson(Map<String, dynamic> json) =>
+//       MoreInformationModel(
+//           id: json["_id"],
+//           name: json["name"],
+//           email: json["email"],
+//           role: json["role"],
+//           phone: json["phone"],
+//           address: json["address"],
+//           privacyPolicyAccepted: json["privacyPolicyAccepted"],
+//           isAdmin: json["isAdmin"],
+//           isVerified: json["isVerified"],
+//           isDeleted: json["isDeleted"],
+//           isBlocked: json["isBlocked"],
+//           if (json['image'] != null) {
+//   image = <Null>[];
+//   json['image'].forEach((v) {
+//   image!.add(new Null.fromJson(v));
+//   });
+//   }
+//           if (
+//    json['driverLicenceFront'] != null) {
+//    driverLicenceFront = <Null>[];
+//    json['driverLicenceFront'].forEach((v) {
+//    driverLicenceFront!.add(new Null.fromJson(v));
+//   });
+//  }
+//   if (json['driverLicenceback'] != null) {
+//   driverLicenceback = <Null>[];
+//   json['driverLicenceback'].forEach((v) {
+//   driverLicenceback!.add(new Null.fromJson(v));
+//   });
+//   }
+//   oneTimeCode = json['oneTimeCode'];
+//   iV = json['__v'];
+//   latitude = json['latitude'];
+//   longitude = json['longitude'];
+// );
+//
+//       Map<String, dynamic> toJson() => {
+//         "_id": id,
+//         "name": name,
+//        "email": email,
+//        "role": role,
+//        "phone": phone,
+//       "address": address,
+//       "privacyPolicyAccepted": privacyPolicyAccepted,
+//       "isAdmin": isAdmin,
+//       "isVerified": isVerified,
+//       "isDeleted": isDeleted,
+//       "isBlocked": isBlocked,
+//
+// };
+// }
+//
+//   class Image {
+//   final dynamic path;
+//   final String? publicFileUrl;
+//
+//   Image({
+//   this.path,
+//   this.publicFileUrl,
+//   });
+//
+//   factory Image.fromJson(Map<String, dynamic> json) => Image(
+//   path: json["path"],
+//   publicFileUrl: json["publicFileURL"],
+//   );
+//
+//   Map<String, dynamic> toJson() => {
+//   "path": path,
+//   "publicFileURL": publicFileUrl,
+//   };
+//   }
